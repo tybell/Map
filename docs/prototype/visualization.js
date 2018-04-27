4341b9c1f870db2bce78b97b67e269c12e0aa581
YvetteWang2015 <yvette.wang2015@gmail.com>
2018-04-27 16:47:07 -0400

    change to dates

--- docs/prototype/visualization.js
+++ docs/prototype/visualization.js
@@ -1,5 +1,5 @@
 (function() {
-    var currYear = 2000;
+    var currYear = moment("2000-01-01","YYYY-MM-DD").unix();
 
     var sizeMin;
     var sizeMax;
@@ -42,7 +42,7 @@
     }
 
     function loadAttackData() {
-        d3.csv("data.csv", function(rows) {
+        d3.csv("data2.csv", function(rows) {
             dataArray = rows;
             // this method will not be called until the above data is fully loaded
             loadGlobalData();
@@ -115,7 +115,8 @@
             .on('mouseover', function(d) {
                 d3.select(this).classed("selected", true)
                 dataArray.forEach(function(entry) {
-                    if (entry.alpha_3_code == d.id && entry.iyear == currYear) {
+                    var item_year = moment(entry.iyear,"YYYY/MM/DD").unix()
+                    if (entry.alpha_3_code == d.id && item_year == currYear) {
                         updatePanel(entry);
                     }
                 });
@@ -183,7 +184,8 @@
     function updatePanelWorld() {
         var d;
         globalData.forEach(function(entry) {
-            if (entry.iyear == currYear) {
+            var item_year = moment(entry.iyear,"YYYY/MM/DD").unix()
+            if (item_year == currYear) {
                 d = entry;
             }
         });
@@ -214,9 +216,9 @@
         var margin = {right: 15, left: 15},
             containerWidth = 840,
             containerHeight = 40;
-        sliderWidth = containerWidth - margin.left - margin.right,
+            sliderWidth = containerWidth - margin.left - margin.right,
             sliderHeight = containerHeight
-        startYear = 2000,
+            startYear = 2000,
             endYear = 2017;
 
         var svgSlider = d3.select("#slider")
@@ -278,7 +280,8 @@
             currYear = selectedYear;
 
             dataArray.forEach(function(entry) {
-                if (entry.iyear == selectedYear) {
+                var item_year = moment(entry.iyear,"YYYY/MM/DD").unix()
+                if (item_year == selectedYear) {
                     selectedYearDataArray.push(entry);
                 }
             });
