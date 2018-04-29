109697984f6b175efa83f794f2eb6e3e8c8e587b
Tyler Bell <Tyler.Bell@ipreo.com>
2018-04-29 14:06:18 -0400

    back to old data

--- docs/prototype/visualization.js
+++ docs/prototype/visualization.js
@@ -43,7 +43,7 @@
     }
 
     function loadAttackData() {
-        d3.csv("food_data2.csv", function(rows) {
+        d3.csv("data.csv", function(rows) {
             dataArray = rows;
             // this method will not be called until the above data is fully loaded
             loadGlobalData();
